2ccac06eb8b3fb26ad9b21a7ec5a0d5a74a65153
Eddy Lardet <lardet_e@epitech.eu>
2017-12-18 12:29:25 +0100

    first step

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  }
+};
